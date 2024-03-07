3707fce634d1df7a204069d74341ccd61a76a0b2
shpetimsedolli <shpeti101@gmail.com>
2024-03-07 09:09:50 +0100

    FinalTest

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [location, setLocation] = useState("");
 
   const API_KEY = "f21b4c899cccd9e952907f3ed4257ac0";
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${API_KEY}`;
 
   const searchLocation = (event) => {
     if (event.key === "Enter") {
